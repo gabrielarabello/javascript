71d7181b865e4e95cfe81b63851a9b4121911e5f
Gabriela Rabelo <129466534+gabrielarabello@users.noreply.github.com>
2024-07-18 16:39:41 -0300

    Exercício atualizado

--- aula11/ex011.js
+++ aula11/ex011.js
@@ -1,8 +1,9 @@
 var idade = 22
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
-    console.log('Não vota!') 
-} else if (idade < 18){
-    {
-        console.log('voto opcional')
-   }
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto Opcional')
+} else {
+    console.log('Voto obrigatório')
 }
