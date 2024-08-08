d617b52ec547c97facf1f3a9aad30f0b5647b168
Gabriela Rabelo <129466534+gabrielarabello@users.noreply.github.com>
2024-08-08 20:13:55 -0300

    atualizado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data =  new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+         //  BOM DIA!
+         img.src = 'fotomanha.png'
+         document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <=18)  {
+         // Boa Tarde!
+        img.src = 'fototarde.png'
+         document.body.style.background = '#b9846f'
+    } else {
+        //Boa noite!
+         img.src= 'fotonoite.png'
+          document.body.style.background = '#515154'
+    }
+}
